d7bdfc75201fb83a1eefee62e040127a7f50d7d2
yelakli <49519709+yelakli@users.noreply.github.com>
2025-05-08 11:30:40 +0100

    vv

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -31,9 +31,9 @@ const Index = () => {
           <p className="text-lg text-gray-600 max-w-2xl mx-auto">
             Association des Oeuvres Sociales des Fonctionnaires de la Région Souss Massa
           </p>
-          <p className="text">La saison estivale de l'année 2024 commence du 22 juin au 15 septembre 2024.</p>
-          <p className="text">Le pourcentage de contribution du participant au processus de camping d'été a été déterminé par :</p>
-          <p className="text">- 25 % pour les salariés classés aux grades 9 et inférieurs, avec cotisation de l'association plafonnée à 700Dh par jour.</p>
+          <p className="text-left">La saison estivale de l'année 2024 commence du 22 juin au 15 septembre 2024.</p>
+          <p className="text-left">Le pourcentage de contribution du participant au processus de camping d'été a été déterminé par :</p>
+          <p className="text-left">- 25 % pour les salariés classés aux grades 9 et inférieurs, avec cotisation de l'association plafonnée à 700Dh par jour.</p>
           <p className="text">- 35 % pour les salariés classés aux grades 10 et supérieurs, avec cotisation de l'association plafonnée à 600Dh par jour.</p>
 
           <p className="text text-gray-600 max-w-2xl mx-auto">Très important:</p>
